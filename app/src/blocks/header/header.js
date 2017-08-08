a08671967e84406b4c42998c86f860ddd1a23efe
R45H <gmr45h@gmail.com>
2017-08-08 17:31:17 +0300

    Some rework desktop submenu

--- app/src/blocks/header/header.js
+++ app/src/blocks/header/header.js
@@ -1,12 +1,15 @@
 var
 	$items = $('.header__menu-item'),
 	subClass = 'header__sub-wrap',
+	visibleClass = 'header__sub-wrap_visible',
 	hoveredClass = 'js-hovered',
 	delay = 300;
 
 /* Наведение курсора на пункт с подменю */
 $items.on('mouseenter', function() {
-	var $this = $(this);
+	var
+		$this = $(this),
+		$sub = $this.find('.' + subClass);
 
 	$this.addClass(hoveredClass);
 
@@ -14,16 +17,17 @@ $items.on('mouseenter', function() {
 
 		if (!$this.hasClass(hoveredClass)) return;
 
-		$this
-			.find('.' + subClass)
-			.slideDown(delay);
+		$sub.slideDown(delay);
+		$sub.addClass(visibleClass);
 	}, delay / 2);
 });
 /* ===== */
 
 /* Увод курсора */
 $items.on('mouseleave', function() {
-	var $this = $(this);
+	var
+		$this = $(this),
+		$sub = $this.find('.' + subClass);
 
 	$this.removeClass(hoveredClass);
 
@@ -31,9 +35,8 @@ $items.on('mouseleave', function() {
 
 		if ($this.hasClass(hoveredClass)) return;
 
-		$this
-			.find('.' + subClass)
-			.slideUp(delay);
+		$sub.slideUp(delay / 2);
+		$sub.removeClass(visibleClass);
 	}, delay * 1.5);
 });
 /* ===== */
