9e4ec607f36ec09722b751c6a5c049547c6d1ea0
R45H <gmr45h@gmail.com>
2017-09-01 19:21:33 +0300

    Add inactive state for the schedule's titles. Add script for it. Complete page schedule

--- app/src/blocks/schedule/schedule.js
+++ app/src/blocks/schedule/schedule.js
@@ -1,13 +1,44 @@
 var
 	classSched = 'schedule', // Класс блока
 	$tabTitles = $('.' + classSched + '__title'), // Заголовки (салоны)
-	classTitleActive = classSched + '__title_active'; // Класс активного заголовка
+	classTitleActive = classSched + '__title_active', // Класс активного заголовка
+	classTitleInactive = classSched + '__title_inactive', // Класс неактивного заголовка
+	$table = $('.' + classSched + '__table'), // Таблицы с расписанием
+	classTableHidden = classSched + '__table_hidden', // Класс скрытой таблицы
+	titleActiveIndex = 0;
+
+// Блокировка заголовков, которые ссылаются на несуществующие таблицы
+$tabTitles.each(function(i) {
+	var
+		$this = $(this),
+		tableId = $this.attr('href').slice(1), // ID таблицы, на которую ссылается заголовок
+		$thisTable = $('#' + tableId); // Таблица
+
+	if (!$thisTable.length) {
+		$this.addClass(classTitleInactive);
+		titleActiveIndex++;
+	} else {
+		if (titleActiveIndex == i) {
+			$thisTable.removeClass(classTableHidden);
+			$this.addClass(classTitleActive);
+		}
+	}
+});
+// =====
 
 $tabTitles.on('click', function(event) {
 	event.preventDefault();
 
-	if ($(this).hasClass(classTitleActive)) return;
+	var
+		$this = $(this),
+		tableId = $this.attr('href').slice(1), // ID таблицы, на которую ссылается заголовок
+		$thisTable = $('#' + tableId); // Таблица
+
+	if ($this.hasClass(classTitleActive) || $this.hasClass(classTitleInactive)) return;
+
+	$table.addClass(classTableHidden);
+	$thisTable.removeClass(classTableHidden);
 
 	$tabTitles.removeClass(classTitleActive);
-	$(this).addClass(classTitleActive);
+	$this.addClass(classTitleActive);
 });
