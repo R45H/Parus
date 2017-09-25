ab41ae57fba6f0642144c19532f249ad7cc7e832
R45H <gmr45h@gmail.com>
2017-09-25 17:13:22 +0300

    Add toggle of salons

--- app/src/blocks/header/header.js
+++ app/src/blocks/header/header.js
@@ -3,6 +3,11 @@ var
 	subClass = 'header__sub-wrap',
 	visibleClass = 'header__sub-wrap_visible',
 	hoveredClass = 'js-hovered',
+	$salonsWrap = $('.header__salons-wrap'),
+	$salonsItem = $('.header__salons-text'),
+	$salonsLinks = $('.header__salons-link'),
+	classSalonsOpened = 'header__salons-text_opened',
+	classSalonsLinksActive = 'header__salons-link_active',
 	delay = 300;
 
 /* Наведение курсора на пункт с подменю */
@@ -39,4 +44,56 @@ $items.on('mouseleave', function() {
 		$sub.removeClass(visibleClass);
 	}, delay * 1.5);
 });
+/* ===== */
+
+/* Установка салона по умолчанию */
+$salonsItem.text($salonsLinks.first().text());
+$salonsLinks.first().addClass(classSalonsLinksActive);
+/* ===== */
+
+/* Клик по элементу с салонами */
+$salonsItem.on('click', function() {
+	toggleSalonsWrap();
+});
+/* ===== */
+
+/* Клик по салону */
+$salonsLinks.on('click', function() {
+	var $this = $(this);
+	if ($this.hasClass(classSalonsLinksActive)) return;
+
+	$salonsItem.text($this.text());
+
+	$salonsLinks.removeClass(classSalonsLinksActive);
+	$this.addClass(classSalonsLinksActive);
+
+	toggleSalonsWrap();
+});
+/* ===== */
+
+/* Закрытие списка салонов при клике вне него */
+$(document).on('click', function(event) {
+	var $target = $(event.target);
+
+	if (!$salonsItem.hasClass(classSalonsOpened) ||
+		$target.parents('.header__salons').length ||
+		$target.hasClass('header__salons-text')) return;
+
+	toggleSalonsWrap();
+});
+/* ===== */
+
+/* Закрытие списка салонов при ресайзе */
+$(window).on('resize', function() {
+	if (window.innerWidth < 576 || !$salonsItem.hasClass(classSalonsOpened)) return;
+
+	toggleSalonsWrap();
+});
+/* ===== */
+
+/* Функция окрытия / закрытия списка салонов */
+function toggleSalonsWrap() {
+	$salonsItem.toggleClass(classSalonsOpened);
+	$salonsWrap.slideToggle(delay / 2);
+}
 /* ===== */
